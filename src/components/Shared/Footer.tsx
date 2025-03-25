334d751a5aa93b1fa9ba994e03b38a5b720dbe38
sajib689 <jibonraja11@gmail.com>
2025-03-25 13:11:17 +0600

    Enhance Footer component with additional menu sections and improved padding

--- src/components/Shared/Footer.tsx
+++ src/components/Shared/Footer.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="bg-[#F7F7F7]">
+    <div className="bg-[#F7F7F7] p-3">
       <div className="container mx-auto  grid grid-cols-1 md:grid-cols-5 lg:grid-cols-5 gap-4 p-4">
         <div>
           <Image src="/logo.png" width={130} height={130} alt="Logo" />
@@ -32,29 +32,89 @@ const Footer = () => {
         <div>
           <h1 className='uppercase text-[14px] font-[600] text-[#333]'>Categories</h1>
           <div className='flex flex-col mt-4 gap-1'>
-            <Link href='' className="cursor-pointer hover:text-blue-500">
+            <Link href='' className="cursor-pointer hover:text-[#ADBDC5]">
             Dhamaka Offer
             </Link>
-            <Link href='' className="cursor-pointer hover:text-blue-500">
+            <Link href='' className="cursor-pointer hover:text-[#ADBDC5]">
             Combo Offer
             </Link>
-            <Link href='' className="cursor-pointer hover:text-blue-500">
+            <Link href='' className="cursor-pointer hover:text-[#ADBDC5]">
             Polo T-shirt
             </Link>
-            <Link href='' className="cursor-pointer hover:text-blue-500">
+            <Link href='' className="cursor-pointer hover:text-[#ADBDC5]">
             T-shirt
             </Link>
-            <Link href='' className="cursor-pointer hover:text-blue-500">
+            <Link href='' className="cursor-pointer hover:text-[#ADBDC5]">
             Shirt
             </Link>
-            <Link href='' className="cursor-pointer hover:text-blue-500">
+            <Link href='' className="cursor-pointer hover:text-[#ADBDC5]">
             Winter Collection
             </Link>
           </div>
         </div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
+        <div>
+          <h1 className='uppercase text-[14px] font-[600] text-[#333]'>Menu</h1>
+          <div className='flex flex-col mt-4 gap-1'>
+            <Link href='' className="cursor-pointer hover:text-[#ADBDC5]">
+            Home
+            </Link>
+            <Link href='' className="cursor-pointer hover:text-[#ADBDC5]">
+            Mens Clothing
+            </Link>
+            <Link href='' className="cursor-pointer hover:text-[#ADBDC5]">
+            women's clothing
+            </Link>
+            <Link href='' className="cursor-pointer hover:text-[#ADBDC5]">
+            About Us
+            </Link>
+            <Link href='' className="cursor-pointer hover:text-[#ADBDC5]">
+            Contact Us
+            </Link>
+            
+          </div>
+        </div>
+        <div>
+          <h1 className='uppercase text-[14px] font-[600] text-[#333]'>Footer Menu</h1>
+          <div className='flex flex-col mt-4 gap-1'>
+            <Link href='' className="cursor-pointer hover:text-[#ADBDC5]">
+            Home
+            </Link>
+            <Link href='' className="cursor-pointer hover:text-[#ADBDC5]">
+            About Us
+            </Link>
+            <Link href='' className="cursor-pointer hover:text-[#ADBDC5]">
+            women's clothing
+            </Link>
+            <Link href='' className="cursor-pointer hover:text-[#ADBDC5]">
+            Mens Clothing
+            </Link>
+            <Link href='' className="cursor-pointer hover:text-[#ADBDC5]">
+            Contact Us
+            </Link>
+            
+          </div>
+        </div>
+        <div>
+          <h1 className='uppercase text-[14px] font-[600] text-[#333]'>Condition</h1>
+          <div className='flex flex-col mt-4 gap-1'>
+            <Link href='' className="cursor-pointer hover:text-[#ADBDC5]">
+            About Us
+            </Link>
+            <Link href='' className="cursor-pointer hover:text-[#ADBDC5]">
+            Delivery Information
+            </Link>
+            <Link href='' className="cursor-pointer hover:text-[#ADBDC5]">
+            Privacy Policy
+            </Link>
+            <Link href='' className="cursor-pointer hover:text-[#ADBDC5]">
+            Terms & Conditions
+            </Link>
+            <Link href='' className="cursor-pointer hover:text-[#ADBDC5]">
+            Returns
+            </Link>
+            
+          </div>
+        </div>
       </div>
     </div>
   );
