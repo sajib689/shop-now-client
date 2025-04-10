4e5ab6ed130c3c19d1ae8fb1919af125146a8586
sajib689 <jibonraja11@gmail.com>
2025-04-10 09:57:09 +0600

    fix an error

--- src/app/order/[id]/page.tsx
+++ src/app/order/[id]/page.tsx
@@ -1,7 +1,12 @@
 
-export default function page({params}: {params: {id: string}}) {
-  const {id} = params
+
+export default function Page({ params }: { params: { id: string } }) {
+  const { id } = params;
+
   return (
-    <div>page: {id}</div>
-  )
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Order ID: {id}</h1>
+      {/* You can add order detail logic here */}
+    </div>
+  );
 }
